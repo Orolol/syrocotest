a9cb24df54c03d1db2ff601069445ff6f98ce27c
Rémy Chabin <remy.chabin@syro.co>
2022-10-28 15:49:06 +0200

    Hotfix/images (#1)
    * add cors package and implement it within server
    
    * add images ressources to public folder
    
    * Store files on S3.
    
    * Remove local images.
    
    Co-authored-by: RB <rayanne.bouslimi@syro.co>

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+var cors = require('cors')
 
 const app = express();
-
 const port = process.env.PORT || 8080;
+
+app.use(cors())
 app.listen(port);
 
 const generateComputationOptionsData = () => {
@@ -13,27 +15,27 @@ const generateComputationOptionsData = () => {
         items: [
           {
             name: 'HMM Helsinki',
-            photoUrL: 'https://upload.wikimedia.org/wikipedia/commons/5/5b/HMM_Helsinki_%282%29_-_Le_Havre.jpg',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/HMM.jpg',
             id: 'hmm_helsinki'
           },
           {
             name: 'MSC Leni',
-            photoUrL: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/MSC_LENI_%2849091698082%29_%28cropped%29.jpg',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/MSC_LENI.jpg',
             id: 'msc-leni'
           },
           {
             name: 'CMA CGM Palais Royal',
-            photoUrL: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/CMA_CGM_PALAIS_ROYAL_%2850863941591%29_%28cropped%29.jpg',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/CMA_CGM_PALAIS_ROYAL.jpg',
             id: 'cma-cgm-palais-royal'
           },
           {
             name: 'Monaco Maersk',
-            photoUrL: 'https://variety.com/wp-content/uploads/2020/12/mank.jpg',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/MONACO.jpg',
             id: 'monaco-maersk'
           },
           {
             name: 'Ever Glory',
-            photoUrL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Aankomst_Ever_Glory%2C_groen_gestuwd_20.000_TEU_containerschip_%2848060356978%29_%28cropped%29.jpg/1024px-Aankomst_Ever_Glory%2C_groen_gestuwd_20.000_TEU_containerschip_%2848060356978%29_%28cropped%29.jpg',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/EVER_GLORY.jpg',
             id: 'ever-glory'
           },
         ],
@@ -44,27 +46,27 @@ const generateComputationOptionsData = () => {
         items: [
           {
             name: 'Eurosal XL',
-            photoUrL: 'https://www.cma-cgm.fr/produits-services/lignes-maritimes/serviceflyermap?serviceCode=WCC',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/eurosal.png',
             id: 'eurosal-xl'
           },
           {
             name: 'Asea Tanzania',
-            photoUrL: 'https://www.cma-cgm.fr/produits-services/lignes-maritimes/serviceflyermap?serviceCode=ASEA',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/asea_tanzania.png',
             id: 'asea-tanzania'
           },
           {
             name: 'North Europe French West Indies',
-            photoUrL: 'https://www.cma-cgm.fr/produits-services/lignes-maritimes/serviceflyermap?serviceCode=NEFWI',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/NEFWI.png',
             id: 'north-europe-french-west-indies'
           },
           {
             name: 'Tanger Med - West Africa',
-            photoUrL: 'https://www.cma-cgm.fr/produits-services/lignes-maritimes/serviceflyermap?serviceCode=EURAF4',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/TMWA.png',
             id: 'tanger-med-west-africa'
           },
           {
             name: 'North Europe - West Africa',
-            photoUrL: 'https://www.cma-cgm.fr/produits-services/lignes-maritimes/serviceflyermap?serviceCode=EURAF1',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/NEWA.png',
             id: 'north-europe-west-africa'
           }
         ],
@@ -75,27 +77,27 @@ const generateComputationOptionsData = () => {
         items: [
           {
             name: 'Norsepower rotor sail',
-            photoUrL: 'https://shipinsightv2.wpenginepowered.com/wp-content/uploads/2020/11/norsepower_tilt-768x512.jpg',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/Norsepower.jpg',
             id: 'norsepower-rotor-sail'
           },
           {
             name: 'Michelin Wisamo',
-            photoUrL: 'https://www.usinenouvelle.com/mediatheque/9/5/3/000976359_896x598_c.png',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/Wisamo.png',
             id: 'michelin-wisamo'
           },
           {
             name: 'Ayro Oceanwings',
-            photoUrL: 'https://assets.meretmarine.com/s3fs-public/styles/large_xl/public/images/2020-12/c4_14.jpg?h=4997dc06&itok=5RN55_mF',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/Oceanwings.jpg',
             id: 'ayro-oceanwings'
           },
           {
             name: 'Beyond the sea Seakite',
-            photoUrL: 'https://www.kiteboarder-mag.com/media/2022/3-mars/beyond_the-sea__fit_687x242.jpg',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/Beyond_The_Sea.jpg',
             id: 'beyond-the-sea-seakite'
           },
           {
             name: 'Aeoldrive Solidsail',
-            photoUrL: 'https://chantiers-atlantique.com/wp-content/uploads/2020/01/ca2fb8133070edb4aae459d2bc25b2b7cfe573b4.png',
+            photoUrL: 'https://syroco-hiring-public-assets.s3.eu-west-1.amazonaws.com/react-take-home-exercise/Aeoldrive.png',
             id: 'aeoldrive-solidsail'
           }
         ],
